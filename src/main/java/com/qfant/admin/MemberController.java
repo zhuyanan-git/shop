deb594a1155c7704fc978fec797e4856a0721389
candyis <30972623@qq.com>
2019-09-20 18:23:16 +0800

    11

--- src/main/java/com/qfant/admin/MemberController.java
+++ src/main/java/com/qfant/admin/MemberController.java
@@ -1,13 +1,13 @@
 package com.qfant.admin;
 
+import com.qfant.framework.AjaxResult;
 import com.qfant.utils.page.TableDataInfo;
+import com.qfant.utils.poi.ExcelUtil;
 import com.qfant.wx.entity.Member;
 import com.qfant.wx.service.MemberService;
 
-import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -30,6 +30,17 @@ public class MemberController extends BaseController{
         List<Member> list = memberService.selectMemberList(member);
         return getDataTable(list);
 
+    }
+
+    /**
+     * 导出家庭成员列表
+     */
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(Member member) {
+        List<Member> list = memberService.selectMemberList(member);
+        ExcelUtil<Member> util = new ExcelUtil<Member>(Member.class);
+        return util.exportExcel(list, "会员信息");
     }
 
 }
